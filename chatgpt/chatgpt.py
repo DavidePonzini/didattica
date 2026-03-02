4993051d35cb00aeabfd596a4798e49f3d3a8e47
Davide Ponzini <davide.ponzini95@gmail.com>
2026-03-02 12:26:08 +0100

    added chatgpt api code

---
+++ chatgpt/chatgpt.py
@@ -0,0 +1,12 @@
+import openai
+from .messages import Message
+
+client = openai.OpenAI(api_key='your-api-key-here')
+
+def generate_answer(messages: Message) -> str:
+    response = client.chat.completions.create(
+        model='gpt-4o-mini',
+        messages=messages.messages
+    )
+
+    return response.choices[0].message.content
